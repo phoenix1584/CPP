81660fa933db4e3ae89ec77f47f869a48645e7ab
phoenix1584 <none@none.com>
2018-03-22 18:04:42 +0530

    #2: rvalue references, urls and skeleton code.

---
+++ FeatureSnippets/t_rvalue_snippet.cc
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// Refs : https://docs.microsoft.com/en-us/cpp/cpp/lvalues-and-rvalues-visual-cpp
+//        https://docs.microsoft.com/en-us/cpp/cpp/rvalue-reference-declarator-amp-amp
+//        http://en.cppreference.com/w/cpp/utility/forward
+//        http://en.cppreference.com/w/cpp/language/reference#Forwarding_references
+//		  http://en.cppreference.com/w/cpp/language/reference_initialization#Lifetime_of_a_temporary
+
+#include <iostream>
+int main(){ 
+}
