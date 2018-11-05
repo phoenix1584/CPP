2293ae7ebca6ea6a01fedfa1dbf2f1a13dfb009b
phoenix1584 <phoenix1584@users.noreply.github.com>
2018-11-05 12:33:22 +0530

    refs #5 : more basic init variations.

--- Concurrency/BasicThread.cc
+++ Concurrency/BasicThread.cc
@@ -1,16 +1,25 @@
 // Copyright (c) 2018 phoenix1584
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 #include <iostream>
 #include <thread>
 
-void ThreadEngine(){
-    std::cout << "Hello from thread!\n";
-}
+void ThreadEngine() { std::cout << "Hello from thread!\n"; }
 
-int main(){
-    std::thread t(ThreadEngine);
-    t.join();
+class Task {
+public:
+  void operator()() const { ThreadEngine(); }
+};
+
+int main() {
+  std::thread t(ThreadEngine);
+  t.join();
+
+  std::thread t1{Task()};
+  t1.join();
+
+  std::thread t2([]{ThreadEngine();});
+  t2.join();
 }
